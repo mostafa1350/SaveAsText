fd6c725570cd1719743389a994415592231d55a2
Mostafa Hosseini <54173709+mostafa1350@users.noreply.github.com>
2025-03-26 19:55:54 +0330

    Finalizing JS
    Finalizing JS

--- myJs.js
+++ myJs.js
@@ -4,8 +4,21 @@ const textArea = document.querySelector("textarea"),
 inputFileName = document.querySelector(".fileName input"),
 selectMenu = document.querySelector(".selectMenu select"),
 btnSave = document.querySelector(".btnSave");
+// =============================
 
+selectMenu.addEventListener("change", ()=>{
+    let selectOption = selectMenu.options[selectMenu.selectedIndex].text;
+    btnSave.innerText = `Save as \" ${selectOption.split(" ")[0]} \"...`
+
+});
 // ==============================================
 btnSave.addEventListener("click",()=>{
-    console.log(textArea.value);
+    console.log(selectMenu.value);
+    //Blob , read as a text or converted to ReadblStream
+    const myBlob = new Blob([textArea.value],{type: selectMenu.value});
+    const fileURL = URL.createObjectURL(myBlob);
+    const myLink = document.createElement("a");
+    myLink.download = inputFileName.value;
+    myLink.href = fileURL;
+    myLink.click();
 });
